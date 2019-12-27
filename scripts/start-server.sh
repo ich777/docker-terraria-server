714848466855661d16bd894bb2f3f407d623fe9d
ich777 <ich777@noreply.example.org>
2019-12-27 14:04:14 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,11 +4,45 @@ CUR_V="$(find $DATA_DIR -name terraria-* | cut -d '-' -f 2,3)"
 echo "---Setting umask to ${UMASK}---"
 umask ${UMASK}
 
+echo "---Checking for 'runtime' folder---"
+if [ ! -d ${SERVER_DIR}/runtime ]; then
+	echo "---'runtime' folder not found, creating...---"
+	mkdir ${SERVER_DIR}/runtime
+else
+	echo "---"runtime" folder found---"
+fi
+
+echo "---Checking if Runtime is installed---"
+if [ -z "$(find ${SERVER_DIR}/runtime -name jre*)" ]; then
+    if [ "${RUNTIME_NAME}" == "basicjre" ]; then
+    	echo "---Downloading and installing Runtime---"
+		cd ${SERVER_DIR}/runtime
+		if wget -q -nc --show-progress --progress=bar:force:noscroll https://github.com/ich777/runtimes/raw/master/jre/basicjre.tar.gz ; then
+			echo "---Successfully downloaded Runtime!---"
+		else
+			echo "---Something went wrong, can't download Runtime, putting server in sleep mode---"
+			sleep infinity
+		fi
+        tar --directory ${SERVER_DIR}/runtime -xvzf ${SERVER_DIR}/runtime/basicjre.tar.gz
+        rm -R ${SERVER_DIR}/runtime/basicjre.tar.gz
+    else
+    	if [ ! -d ${SERVER_DIR}/runtime/${RUNTIME_NAME} ]; then
+        	echo "---------------------------------------------------------------------------------------------"
+        	echo "---Runtime not found in folder 'runtime' please check again! Putting server in sleep mode!---"
+        	echo "---------------------------------------------------------------------------------------------"
+        	sleep infinity
+        fi
+    fi
+else
+	echo "---Runtime found---"
+fi
+
+
 echo "---Version Check---"
 if [ ! -d "${SERVER_DIR}/lib" ]; then
     echo "---Terraria not found, downloading!---"
     cd ${SERVER_DIR}
-    wget -q - http://terraria.org/server/terraria-server-$LAT_V.zip
+    wget -nc --show-progress --progress=bar:force:noscroll -q - http://terraria.org/server/terraria-server-$LAT_V.zip
     unzip -q ${SERVER_DIR}/terraria-server-$LAT_V.zip
     mv ${SERVER_DIR}/$LAT_V/Linux/* ${SERVER_DIR}
     rm -R ${SERVER_DIR}/$LAT_V
@@ -18,7 +52,7 @@ elif [ "$LAT_V" != "$CUR_V" ]; then
     echo "---Newer version found, installing!---"
     rm ${DATA_DIR}/terraria-$CUR_V
     cd ${SERVER_DIR}
-    wget -q - http://terraria.org/server/terraria-server-$LAT_V.zip
+    wget -nc --show-progress --progress=bar:force:noscroll -q - http://terraria.org/server/terraria-server-$LAT_V.zip
     unzip -q ${SERVER_DIR}/terraria-server-$LAT_V.zip
     mv ${SERVER_DIR}/$LAT_V/Linux/* ${SERVER_DIR}
     rm -R ${SERVER_DIR}/$LAT_V
@@ -34,33 +68,37 @@ fi
 echo "---sleep---"
 sleep infinity
 
-CUR_MOD_V="$(find ${DATA_DIR} -name tshock_* | cut -d '_' -f2)"
-LAT_MOD_V="$(curl -s https://api.github.com/repos/Pryaxis/TShock/releases/latest | grep tag_name | cut -d '"' -f4 | cut -d 'v' -f2)"
+CUR_MOD_V="$(find ${DATA_DIR} -name tmodloader_* | cut -d '_' -f2)"
+LAT_MOD_V="$(curl -s https://api.github.com/repos/tModLoader/tModLoader/releases/latest | grep tag_name | cut -d '"' -f4 | cut -d 'v' -f2)"
 
-echo "---Version Check of TShock Mod---"
+echo "---Version Check of tModloader---"
 if [ -z "$CUR_MOD_V" ]; then
-    echo "---TShock Mod not found! Downloading...---"
+    echo "---tModloader not found! Downloading...---"
     cd ${SERVER_DIR}
-    curl -s https://api.github.com/repos/Pryaxis/TShock/releases/latest \
+    curl -s https://api.github.com/repos/tModLoader/tModLoader/releases/latest \
     | grep "browser_download_url." \
+    | grep "tModLoader.Linux.*" \
     | cut -d ":" -f2,3 \
     | cut -d '"' -f2 \
-    | wget -qi -
-    unzip -qo /serverdata/serverfiles/tshock_$LAT_MOD_V.zip
-    mv ${SERVER_DIR}/tshock_$LAT_MOD_V.zip ${DATA_DIR}/tshock_$LAT_MOD_V
+    | wget -nc --show-progress --progress=bar:force:noscroll -qi -
+    tar --overwrite -xvf ${SERVER_DIR}/tModLoader.Linux.v$LAT_MOD_V.tar.gz
+    rm ${SERVER_DIR}/tModLoader.Linux.v$LAT_MOD_V.tar.gz
+    touch ${DATA_DIR}/tmodloader_$LAT_MOD_V
 elif [ "$LAT_MOD_V" != "$CUR_MOD_V" ]; then
     echo "---Newer version found, installing!---"
-    rm ${DATA_DIR}/tshock_$CUR_MOD_V
+    rm ${DATA_DIR}/tmodloader_$CUR_MOD_V
     cd ${SERVER_DIR}
-    curl -s https://api.github.com/repos/Pryaxis/TShock/releases/latest \
+    curl -s https://api.github.com/repos/tModLoader/tModLoader/releases/latest \
     | grep "browser_download_url." \
+    | grep "tModLoader.Linux.*" \
     | cut -d ":" -f2,3 \
     | cut -d '"' -f2 \
-    | wget -qi -
-    unzip -qo /serverdata/serverfiles/tshock_$LAT_MOD_V.zip
-    mv ${SERVER_DIR}/tshock_$LAT_MOD_V.zip ${DATA_DIR}/tshock_$LAT_MOD_V
+    | wget -nc --show-progress --progress=bar:force:noscroll -qi -
+    tar --overwrite -xvf ${SERVER_DIR}/tModLoader.Linux.v$LAT_MOD_V.tar.gz
+    rm ${SERVER_DIR}/tModLoader.Linux.v$LAT_MOD_V.tar.gz
+    touch ${DATA_DIR}/tmodloader_$LAT_MOD_V
 elif [ "$LAT_MOD_V" == "$CUR_MOD_V" ]; then
-    echo "---TShock Mod Version up-to-date---"
+    echo "---tModloader Version up-to-date---"
 else
     echo "---Something went wrong, putting server in sleep mode---"
     sleep infinity
@@ -71,7 +109,7 @@ echo "---Prepare Server---"
 if [ ! -f "${SERVER_DIR}/serverconfig.txt" ]; then
   echo "---No serverconfig.txt found, downloading...---"
   cd ${SERVER_DIR}
-  wget -qi serverconfig.txt "https://raw.githubusercontent.com/ich777/docker-terraria-server/master/config/serverconfig.txt"
+  wget -nc --show-progress --progress=bar:force:noscroll -qi serverconfig.txt "https://raw.githubusercontent.com/ich777/docker-terraria-server/master/config/serverconfig.txt"
 fi
 echo "---Server ready---"
 chmod -R 777 ${DATA_DIR}
