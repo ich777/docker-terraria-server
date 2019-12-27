b3420fd1eb1a7a91d6bed536987ed699501cd7fe
ich777 <ich777@noreply.example.org>
2019-12-27 14:20:56 +0100

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -4,40 +4,6 @@ CUR_V="$(find $DATA_DIR -name terraria-* | cut -d '-' -f 2,3)"
 echo "---Setting umask to ${UMASK}---"
 umask ${UMASK}
 
-echo "---Checking for 'runtime' folder---"
-if [ ! -d ${SERVER_DIR}/runtime ]; then
-	echo "---'runtime' folder not found, creating...---"
-	mkdir ${SERVER_DIR}/runtime
-else
-	echo "---"runtime" folder found---"
-fi
-
-echo "---Checking if Runtime is installed---"
-if [ -z "$(find ${SERVER_DIR}/runtime -name jre*)" ]; then
-    if [ "${RUNTIME_NAME}" == "basicjre" ]; then
-    	echo "---Downloading and installing Runtime---"
-		cd ${SERVER_DIR}/runtime
-		if wget -q -nc --show-progress --progress=bar:force:noscroll https://github.com/ich777/runtimes/raw/master/jre/basicjre.tar.gz ; then
-			echo "---Successfully downloaded Runtime!---"
-		else
-			echo "---Something went wrong, can't download Runtime, putting server in sleep mode---"
-			sleep infinity
-		fi
-        tar --directory ${SERVER_DIR}/runtime -xvzf ${SERVER_DIR}/runtime/basicjre.tar.gz
-        rm -R ${SERVER_DIR}/runtime/basicjre.tar.gz
-    else
-    	if [ ! -d ${SERVER_DIR}/runtime/${RUNTIME_NAME} ]; then
-        	echo "---------------------------------------------------------------------------------------------"
-        	echo "---Runtime not found in folder 'runtime' please check again! Putting server in sleep mode!---"
-        	echo "---------------------------------------------------------------------------------------------"
-        	sleep infinity
-        fi
-    fi
-else
-	echo "---Runtime found---"
-fi
-
-
 echo "---Version Check---"
 if [ ! -d "${SERVER_DIR}/lib" ]; then
     echo "---Terraria not found, downloading!---"
@@ -64,9 +30,6 @@ else
   echo "---Something went wrong, putting server in sleep mode---"
   sleep infinity
 fi
-
-echo "---sleep---"
-sleep infinity
 
 CUR_MOD_V="$(find ${DATA_DIR} -name tmodloader_* | cut -d '_' -f2)"
 LAT_MOD_V="$(curl -s https://api.github.com/repos/tModLoader/tModLoader/releases/latest | grep tag_name | cut -d '"' -f4 | cut -d 'v' -f2)"
@@ -104,7 +67,6 @@ else
     sleep infinity
 fi
 
-
 echo "---Prepare Server---"
 if [ ! -f "${SERVER_DIR}/serverconfig.txt" ]; then
   echo "---No serverconfig.txt found, downloading...---"
@@ -116,11 +78,8 @@ chmod -R 777 ${DATA_DIR}
 echo "---Checking for old logs---"
 find ${SERVER_DIR} -name "masterLog.*" -exec rm -f {} \;
 
-
 echo "---Start Server---"
 cd ${SERVER_DIR}
-screen -S Terraria -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m \
-    mono-sgen TerrariaServer.exe \
-    ${GAME_PARAMS}
+screen -S Terraria -L -Logfile ${SERVER_DIR}/masterLog.0 -d -m ${SERVER_DIR}/tModLoaderServer -config serverconfig.txt ${GAME_PARAMS}
 sleep 2
 tail -f ${SERVER_DIR}/masterLog.0
