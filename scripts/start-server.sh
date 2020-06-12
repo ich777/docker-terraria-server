8d93d20eb6151b6900da1766f5ce671e9cb2a08c
ich777 <ich777@noreply.example.org>
2020-06-12 08:54:46 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
 sleep infinity
-CUR_MOD_V="$(find ${SERVER_DIR} -name tshock_* | cut -d '_' -f2)"
-LAT_MOD_V="$(curl -s https://api.github.com/repos/Pryaxis/TShock/releases/latest | grep tag_name | cut -d '"' -f4 | cut -d 'v' -f2)"
+CUR_MOD_V="$(find ${SERVER_DIR} -name tshock_* 2>/dev/null | cut -d '_' -f2)" 
+LAT_MOD_V="$(curl -s https://api.github.com/repos/Pryaxis/TShock/releases | grep tag_name | cut -d '"' -f4 | cut -d 'v' -f2 | head -1)"
 
 echo "---Version Check of TShock Mod---"
 if [ -z "$CUR_MOD_V" ]; then
     echo "---TShock Mod not found! Downloading...---"
     cd ${SERVER_DIR}
-    curl -s https://api.github.com/repos/Pryaxis/TShock/releases/latest \
+    curl -s https://api.github.com/repos/Pryaxis/TShock/releases \
     | grep "browser_download_url." \
     | cut -d ":" -f2,3 \
     | cut -d '"' -f2 \
+	| head -1 \
     | wget -qi -
     unzip -qo /serverdata/serverfiles/tshock_$LAT_MOD_V.zip
     mv ${SERVER_DIR}/tshock_$LAT_MOD_V.zip ${SERVER_DIR}/tshock_$LAT_MOD_V
+	touch ${SERVER_DIR}/tshock_$LAT_MOD_V
 elif [ "$LAT_MOD_V" != "$CUR_MOD_V" ]; then
     echo "---Newer version found, installing!---"
     rm ${SERVER_DIR}/tshock_$CUR_MOD_V
     cd ${SERVER_DIR}
-    curl -s https://api.github.com/repos/Pryaxis/TShock/releases/latest \
+    curl -s https://api.github.com/repos/Pryaxis/TShock/releases \
     | grep "browser_download_url." \
     | cut -d ":" -f2,3 \
     | cut -d '"' -f2 \
+	| head -1 \
     | wget -qi -
     unzip -qo /serverdata/serverfiles/tshock_$LAT_MOD_V.zip
     mv ${SERVER_DIR}/tshock_$LAT_MOD_V.zip ${SERVER_DIR}/tshock_$LAT_MOD_V
+	touch ${SERVER_DIR}/tshock_$LAT_MOD_V
 elif [ "$LAT_MOD_V" == "$CUR_MOD_V" ]; then
     echo "---TShock Mod Version up-to-date---"
 else
@@ -33,9 +37,6 @@ else
 fi
 
 echo "---Prepare Server---"
-if [ -f ${SERVER_DIR}/System.dll ]; then
-	rm ${SERVER_DIR}/System.dll
-fi
 if [ ! -f "${SERVER_DIR}/serverconfig.txt" ]; then
   echo "---No serverconfig.txt found, downloading...---"
   cd ${SERVER_DIR}
