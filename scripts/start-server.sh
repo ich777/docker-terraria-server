9f7e6bdfb7bebe1d005264a81c35713520841c30
ich777 <ich777@noreply.example.org>
2020-06-12 08:18:12 +0200

    „scripts/start-server.sh“ ändern

--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,47 +1,5 @@
 #!/bin/bash
-CUR_V="$(grep "Version" /serverdata/serverfiles/changelog.txt 2>/dev/null | head -1 | cut -d ' ' -f 2)"
-CUR_V="${CUR_V//./}"
-DL_LINK=https://terraria.org$(curl -sL https://terraria.org/ | grep -Eo $'[^\'"]+terraria-server-[^\'"]+')
-DL_TOP=?${DL_LINK##*\?}
-LAT_V="$(echo ${DL_LINK##*-} | cut -d '.' -f 1)"
-DL_LINK=${DL_LINK%terraria*}
-
-echo "---Version Check---"
-if [ ! -d "${SERVER_DIR}/lib" ]; then
-   	echo "---Terraria not found, downloading!---"
-   	cd ${SERVER_DIR}
-   	if wget -q -nc --show-progress --progress=bar:force:noscroll -O terraria-server-$LAT_V.zip "$DL_LINK"terraria-server-$LAT_V.zip"$DL_TOP" ; then
-		echo "---Successfully downloaded Terraria---"
-	else
-		echo "------------------------------------------------------------------------------"
-		echo "------------Can't download Terraria, putting server into sleep mode-----------"
-		echo "------------------------------------------------------------------------------"
-	fi
-    unzip -q ${SERVER_DIR}/terraria-server-$LAT_V.zip
-    cp -R -f ${SERVER_DIR}/$LAT_V/Linux/* ${SERVER_DIR}
-    rm -R ${SERVER_DIR}/$LAT_V
-elif [ "$LAT_V" != "$CUR_V" ]; then
-    echo "---Newer version found, installing!---"
-    rm ${SERVER_DIR}/terraria-$CUR_V
-    cd ${SERVER_DIR}
-    if wget -q -nc --show-progress --progress=bar:force:noscroll -O terraria-server-$LAT_V.zip "$DL_LINK"terraria-server-$LAT_V.zip"$DL_TOP" ; then
-		echo "---Successfully downloaded Terraria---"
-	else
-		echo "------------------------------------------------------------------------------"
-		echo "------------Can't download Terraria, putting server into sleep mode-----------"
-		echo "------------------------------------------------------------------------------"
-	fi
-    unzip -q ${SERVER_DIR}/terraria-server-$LAT_V.zip
-    cp -R -f ${SERVER_DIR}/$LAT_V/Linux/* ${SERVER_DIR}
-    rm -R ${SERVER_DIR}/$LAT_V
-    rm -R ${SERVER_DIR}/terraria-server-$LAT_V.zip
-elif [ "$LAT_V" == "$CUR_V" ]; then
-    echo "---Terraria Version up-to-date---"
-else
- 	echo "---Something went wrong, putting server in sleep mode---"
- 	sleep infinity
-fi
-
+sleep infinity
 CUR_MOD_V="$(find ${SERVER_DIR} -name tshock_* | cut -d '_' -f2)"
 LAT_MOD_V="$(curl -s https://api.github.com/repos/Pryaxis/TShock/releases/latest | grep tag_name | cut -d '"' -f4 | cut -d 'v' -f2)"
 
