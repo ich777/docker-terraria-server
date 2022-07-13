c73d77a0d0435adab13dc1634b6d447c3bb81840
Christoph <christophhummer@gmail.com>
2022-07-13 17:56:05 +0200

    Revert "Terraria Mobile Support"

--- README.md
+++ README.md
@@ -1,34 +1,32 @@
-# Terraria Mobile Dedicated Server in Docker optimized for Unraid
+# Terraria Dedicated Server in Docker optimized for Unraid
 
-This Docker will download and install the Terraria Mobile Server and run it.
+This Docker will download and install Terraria and run it.
 
 ***SERVER PASSWORD: Docker***
 
 Update Notice: The Container will check on every start/restart if there is a newer version of the game available and install it if so.
 
-WEB CONSOLE: You can connect to the Terraria Mobile Server console by opening your browser and go to HOSTIP:9013 (eg: 192.168.1.1:9013) or click on WebUI on the Docker page within Unraid.
+WEB CONSOLE: You can connect to the Terraria console by opening your browser and go to HOSTIP:9012 (eg: 192.168.1.1:9012) or click on WebUI on the Docker page within Unraid.
 
 ## Env params
 
-| Name        | Value                          | Example                  |
-| ----------- | ------------------------------ | ------------------------ |
-| SERVER_DIR  | Folder for gamefiles           | /serverdata/serverfiles  |
+| Name | Value | Example |
+| --- | --- | --- |
+| SERVER_DIR | Folder for gamefiles | /serverdata/serverfiles |
 | GAME_PARAMS | Commandline startup parameters | -config serverconfig.txt |
-| UID         | User Identifier                | 99                       |
-| GID         | Group Identifier               | 100                      |
+| UID | User Identifier | 99 |
+| GID | Group Identifier | 100 |
 
 # Run example
 
-```bash
-docker run --name TerrariaMobile -d \
-    -p 7777:7777/udp -p 9013:8080 \
+docker run --name Terraria -d \
+    -p 7777:7777/udp -p 9012:8080 \
     --env 'GAME_PARAMS=-config serverconfig.txt' \
     --env 'UID=99' \
     --env 'GID=100' \
-    --volume /path/to/terraria-mobile:/serverdata/serverfiles \
+    --volume /mnt/user/appdata/terraria:/serverdata/serverfiles \
     --restart=unless-stopped \
-    ich777/terrariaserver:mobile
-```
+    ich777/terrariaserver:latest
 
 This Docker was mainly created for the use with Unraid, if you don’t use Unraid you should definitely try it!
 
--- scripts/start-server.sh
+++ scripts/start-server.sh
@@ -1,45 +1,47 @@
 #!/bin/bash
 CUR_V="$(grep "Version" /serverdata/serverfiles/changelog.txt 2>/dev/null | head -1 | cut -d ' ' -f 2)"
+CUR_V="${CUR_V//./}"
 # Deprecated since Terraria changed how to get the download URL again
 #DL_LINK=https://terraria.org$(curl -sL https://terraria.org/ | grep -Eo $'[^\'"]+terraria-server-[^\'"]+')
 #DL_TOP=?${DL_LINK##*\?}
 #LAT_V="$(echo ${DL_LINK##*-} | cut -d '.' -f 1)"
 #DL_LINK=${DL_LINK%terraria*}
+LAT_V="${TERRARIA_SRV_V//./}"
 
-rm -rf ${SERVER_DIR}/Terraria-Mobile-Server-*.zip
+rm -rf ${SERVER_DIR}/terraria-server-*.zip
 
 echo "---Version Check---"
 if [ ! -d "${SERVER_DIR}/lib" ]; then
-	echo "---Terraria Mobile not found, downloading!---"
+   	echo "---Terraria not found, downloading!---"
    	cd ${SERVER_DIR}
-   	if wget -q -nc --show-progress --progress=bar:force:noscroll -O Terraria-Mobile-Server-$TERRARIA_MOBILE_SRV_V.zip "https://www.terraria.org/api/download/mobile-dedicated-server/Terraria-Mobile-Server-${TERRARIA_MOBILE_SRV_V}.zip" ; then
+   	if wget -q -nc --show-progress --progress=bar:force:noscroll -O terraria-server-$LAT_V.zip "https://terraria.org/api/download/pc-dedicated-server/terraria-server-${LAT_V}.zip" ; then
 		echo "---Successfully downloaded Terraria---"
 	else
 		echo "------------------------------------------------------------------------------"
-		echo "--------Can't download Terraria Mobile, putting server into sleep mode--------"
+		echo "------------Can't download Terraria, putting server into sleep mode-----------"
 		echo "------------------------------------------------------------------------------"
 		sleep infinity
 	fi
-    unzip -qo ${SERVER_DIR}/Terraria-Mobile-Server-$TERRARIA_MOBILE_SRV_V.zip
-    cp -R -f ${SERVER_DIR}/Terraria-Mobile-Server-$TERRARIA_MOBILE_SRV_V/Linux/* ${SERVER_DIR}/
-    rm -R ${SERVER_DIR}/Terraria-Mobile-Server-$TERRARIA_MOBILE_SRV_V.zip ${SERVER_DIR}/Terraria-Mobile-Server-$TERRARIA_MOBILE_SRV_V
-elif [ "$TERRARIA_MOBILE_SRV_V" != "$CUR_V" ]; then
+    unzip -qo ${SERVER_DIR}/terraria-server-$LAT_V.zip
+    cp -R -f ${SERVER_DIR}/$LAT_V/Linux/* ${SERVER_DIR}
+    rm -R ${SERVER_DIR}/terraria-server-$LAT_V.zip
+elif [ "$LAT_V" != "$CUR_V" ]; then
     echo "---Newer version found, installing!---"
-   	cd ${SERVER_DIR}
-   	if wget -q -nc --show-progress --progress=bar:force:noscroll -O Terraria-Mobile-Server-$TERRARIA_MOBILE_SRV_V.zip "https://www.terraria.org/api/download/mobile-dedicated-server/Terraria-Mobile-Server-${TERRARIA_MOBILE_SRV_V}.zip" ; then
+    cd ${SERVER_DIR}
+   	if wget -q -nc --show-progress --progress=bar:force:noscroll -O terraria-server-$LAT_V.zip "https://terraria.org/api/download/pc-dedicated-server/terraria-server-${LAT_V}.zip" ; then
 		echo "---Successfully downloaded Terraria---"
 	else
 		echo "------------------------------------------------------------------------------"
-		echo "--------Can't download Terraria Mobile, putting server into sleep mode--------"
+		echo "------------Can't download Terraria, putting server into sleep mode-----------"
 		echo "------------------------------------------------------------------------------"
 		sleep infinity
 	fi
-    unzip -qo ${SERVER_DIR}/Terraria-Mobile-Server-$TERRARIA_MOBILE_SRV_V.zip
-    cp -R -f ${SERVER_DIR}/Terraria-Mobile-Server-$TERRARIA_MOBILE_SRV_V/Linux/* ${SERVER_DIR}/
-    rm -R ${SERVER_DIR}/Terraria-Mobile-Server-$TERRARIA_MOBILE_SRV_V.zip ${SERVER_DIR}/Terraria-Mobile-Server-$TERRARIA_MOBILE_SRV_V
-elif [ "$TERRARIA_MOBILE_SRV_V" == "$CUR_V" ]; then
-    echo "---Terraria Server v$TERRARIA_MOBILE_SRV_V up-to-date---"
-	echo "---If you want to change the version add a Variable with the Key: 'TERRARIA_MOBILE_SRV_V' and the Value eg: '1.4.2.3'."
+    unzip -qo ${SERVER_DIR}/terraria-server-$LAT_V.zip
+    cp -R -f ${SERVER_DIR}/$LAT_V/Linux/* ${SERVER_DIR}
+    rm -R ${SERVER_DIR}/terraria-server-$LAT_V.zip
+elif [ "$LAT_V" == "$CUR_V" ]; then
+    echo "---Terraria Server v$TERRARIA_SRV_V up-to-date---"
+	echo "---If you want to change the version add a Variable with the Key: 'TERRARIA_SRV_V' and the Value eg: '1.4.2.3'."
 else
  	echo "---Something went wrong, putting server in sleep mode---"
  	sleep infinity
@@ -51,8 +53,17 @@ if [ ! -f ~/.screenrc ]; then
 bindkey \"^C\" echo 'Blocked. Please use to command \"exit\" to shutdown the server or close this window to exit the terminal.'" > ~/.screenrc
 fi
 if [ ! -f "${SERVER_DIR}/serverconfig.txt" ]; then
-  echo "---No serverconfig.txt found, copying...---"
-  cp -f /config/serverconfig.txt ${SERVER_DIR}
+  echo "---No serverconfig.txt found, downloading...---"
+  cd ${SERVER_DIR}
+  wget -qi -O serverconfig.txt "https://raw.githubusercontent.com/ich777/docker-terraria-server/master/config/serverconfig.txt"
+fi
+if [ ! -d ${SERVER_DIR}/Worlds ]; then
+	echo "---No World found, downloading---"
+	mkdir ${SERVER_DIR}/Worlds
+	cd ${SERVER_DIR}/Worlds
+	wget -qi -O ${SERVER_DIR}/Worlds/world.zip "https://raw.githubusercontent.com/ich777/docker-terraria-server/master/world.zip"
+	unzip ${SERVER_DIR}/Worlds/world.zip
+	rm ${SERVER_DIR}/Worlds/world.zip
 fi
 echo "---Server ready---"
 chmod -R ${DATA_PERM} ${DATA_DIR}
